0da04c0afb4b1802d70d4a366468289d03ee401e
Lydia-Liu522 <3544473931@qq.com>
2024-05-28 15:25:41 +0800

    修改了shopping cart以及wishlist中的图片，以及更换了logo (#11)

--- html/lib/home.js
+++ html/lib/home.js
@@ -4,20 +4,21 @@
 	//---------------------------------------------
 	//Nivo slider
 	//---------------------------------------------
-		$('#ensign-nivoslider').nivoSlider({
-			effect: 'random',
-			slices: 15,
-			boxCols: 8,
-			boxRows: 4,
-			animSpeed: 700,
-			pauseTime: 9000,
-			startSlide: 0,
-			directionNav: true,
-			controlNavThumbs: false,
-			pauseOnHover: false,
-			controlNav: true,
-			prevText: '<i class="zmdi zmdi-chevron-left"></i>',
-			nextText: '<i class="zmdi zmdi-chevron-right"></i>'
-		});
+	$('#ensign-nivoslider').nivoSlider({
+		effect: 'random',                // Animation effect, 'random' means randomly chosen animation effect
+		slices: 15,                      // Number of slices, used for certain animation effects like slice
+		boxCols: 8,                      // Number of box columns, used for box animation effects
+		boxRows: 4,                      // Number of box rows, used for box animation effects
+		animSpeed: 700,                  // Animation speed, in milliseconds
+		pauseTime: 9000,                 // Time each slide is shown, in milliseconds
+		startSlide: 0,                   // Starting slide index, 0 means the first slide
+		directionNav: true,              // Whether to display left and right navigation arrows
+		controlNavThumbs: false,         // Whether to display thumbnail navigation
+		pauseOnHover: false,             // Whether to pause the slider when hovering
+		controlNav: true,                // Whether to display navigation controls (dots navigation)
+		prevText: '<i class="zmdi zmdi-chevron-left"></i>', // Custom HTML content for the previous button
+		nextText: '<i class="zmdi zmdi-chevron-right"></i>' // Custom HTML content for the next button
+	});
+	
 
 })(jQuery); 
